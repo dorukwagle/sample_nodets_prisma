972b911b27bf47fe6f6d648370ea994f29b11cfb
dorukwagle <dorukwagle@gmail.com>
2024-12-22 14:22:36 +0545

    use paginator

--- server/src/api/sample/samplesModel.ts
+++ server/src/api/sample/samplesModel.ts
@@ -5,6 +5,7 @@ import prismaClient from "../../utils/prismaClient";
 import Sample, { SampleType } from "../../validations/Sample";
 import PaginationParams, { PaginationParamsType } from "../../validations/PaginationParams";
 import { DEFAULT_PAGE_SIZE } from "../../entities/constants";
+import { getPaginatedItems } from "../../utils/paginator";
 
 
 const createSample = async (userId: string, body: SampleType) => {
@@ -39,75 +40,17 @@ const deleteSample = async (userId: string, sampleId: string) => {
     });
 };
 
-const getFilter = (userId: string, filter: PaginationParamsType) => {
-    const seed = filter.seed;
-    const status = filter.status;
-
-    const noSeed = { userId, status };
-    const withSeed = {
-        AND: [
-            {
-                userId,
-                status,
-                OR: [
-                    {
-                        title: {
-                            search: seed,
-                        },
-                        body: {
-                            search: seed,
-                        },
-                    },
-                ],
-            },
+const paginateSamples = async (userId: string, body: PaginationParamsType): Promise<PaginationReturnTypes> => {
+    
+    return getPaginatedItems("samples", body, { 
+        defaultSeed: body.seed || "test sample",
+        fields: [
+            { column: "userId", seed: userId },
+            { column: "title" },
+            { column: "body", search: true },
         ],
-    };
-
-    return seed ? withSeed : noSeed;
-};
-
-const paginateSamples = async (userId: string, body: PaginationParamsType) => {
-    const res = { statusCode: 200, info: {}, data: {}} as PaginationReturnTypes;
-
-    const validation = PaginationParams.safeParse(body);
-    const error = formatValidationErrors(validation);
-    if (error) {
-        res.statusCode = error.statusCode;
-        res.error = error.error;
-        return res;
-    };
-
-    const filter = validation.data!;
-
-    const page = filter.page || 1;
-    const pageSize = filter.pageSize || DEFAULT_PAGE_SIZE;
-
-
-    res.data = await prismaClient.samples.findMany({
-        where: getFilter(userId, filter),
-        orderBy: filter.seed
-            ? {
-                  _relevance: {
-                      fields: ["title", "body"],
-                      search: filter.seed || "",
-                      sort: "asc",
-                  },
-              }
-            : {
-                  createdAt: "desc",
-              },
-        skip: (page - 1) * pageSize,
-        take: pageSize,
-    });
-
-    const total = await prismaClient.samples.count({
-        where: getFilter(userId, filter),
-    });
-
-    res.info.itemsCount = total;
-    res.info.hasNextPage = total > (page * pageSize);
-
-    return res;
+        operator: "AND",
+     }, [], { createdAt: "desc" });
 };
 
 export { createSample, deleteSample, paginateSamples };
--- server/src/utils/paginator.ts
+++ server/src/utils/paginator.ts
@@ -3,7 +3,7 @@ import PaginationReturnTypes from "../entities/PaginationReturnTypes";
 import { DEFAULT_PAGE_SIZE } from "../entities/constants";
 import prismaClient from "./prismaClient";
 
-type model = "genres" | "publishers" | "authors" | "bookInfo" | "bookReservations";
+type model = "samples" | "users";
 
 export type WhereArgs = {
     fields: { column: string, child?: string, search?: boolean, seed?: any, oneToMany?: boolean, number?: boolean }[],
